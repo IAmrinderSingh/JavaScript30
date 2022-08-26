00978bde4133abc44a33312ce0b8db1cfb31d02f
IAmrinderSingh <amrindersingh1703@gmail.com>
2022-08-26 12:11:12 +0530

    Selecting all vedio methods

---
+++ Day11 Custom Video Player/scripts.js
@@ -0,0 +1,8 @@
+// Get our methods
+const player=document.querySelector('.player');
+const video=document.querySelector('.viewer');
+const progress=document.querySelector('.progress');
+const progressBar=document.querySelector('.progress_filled');
+const toggle=document.querySelector('.toggle');
+const ranges=document.querySelector('.player__slider');
+const skipButtons=document.querySelector('[data-skip]');
