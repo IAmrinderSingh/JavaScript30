15fedc1fb2d6d179aa1f8af070cc4a2149db06dc
IAmrinderSingh <amrindersingh1703@gmail.com>
2022-08-26 12:23:52 +0530

    Play and pause vedio using toogle button

--- Day11 Custom Video Player/scripts.js
+++ Day11 Custom Video Player/scripts.js
@@ -6,3 +6,13 @@ const progressBar=document.querySelector('.progress_filled');
 const toggle=document.querySelector('.toggle');
 const ranges=document.querySelector('.player__slider');
 const skipButtons=document.querySelector('[data-skip]');
+
+// Build our functions
+
+function toogle(){
+    console.log(video);
+    const method=video.paused ? 'play':'pause';
+    video[method]();
+}
+
+toggle.addEventListener('click',toogle);
