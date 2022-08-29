da7c2c9b5daa3e0b3aa95dc422f9e19c85eb4859
IAmrinderSingh <amrindersingh1703@gmail.com>
2022-08-29 12:22:16 +0530

    Progress bar added

--- Day11 Custom Video Player/scripts.js
+++ Day11 Custom Video Player/scripts.js
@@ -2,7 +2,7 @@
 const player=document.querySelector('.player');
 const video=document.querySelector('.viewer');
 const progress=document.querySelector('.progress');
-const progressBar=document.querySelector('.progress_filled');
+const progressBar=document.querySelector('.progress__filled');
 const toggle=document.querySelector('.toggle');
 const ranges=document.querySelectorAll('.player__slider');
 const skipButtons=document.querySelectorAll('[data-skip]');
@@ -26,6 +26,11 @@ function handleRangeUpdate(){
     video[this.name]=this.value;
 }
 
+function handleProgress(){
+    const percent=(video.currentTime / video.duration)*100;
+    progressBar.style.flexBasis=`${percent}%`;
+}
+
 ranges.forEach(range => range.addEventListener('change',handleRangeUpdate));
 ranges.forEach(range => range.addEventListener('mousemove',handleRangeUpdate));
 
@@ -33,6 +38,7 @@ skipButtons.forEach(button => button.addEventListener('click',skip));
 
 video.addEventListener('play',updateButton);
 video.addEventListener('pause',updateButton);
+video.addEventListener('timeupdate',handleProgress);
 
 video.addEventListener('click',toogle);
 toggle.addEventListener('click',toogle);
